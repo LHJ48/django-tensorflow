7ab3c6ea7bae498c49d3db626da7ea7c90a11669
LeeHoJun4880 <ghwns23@gmail.com>
2021-06-19 14:41:35 +0900

    second commit

--- cctv_crime/modules.py
+++ cctv_crime/modules.py
@@ -4,6 +4,8 @@ import pandas as pd
 import numpy as np
 import json
 import googlemaps
+
+
 @dataclass
 class File(object):
     context: str
@@ -67,10 +69,10 @@ class Reader(ReaderBase):
         return json.load(open(f'{self.new_file((file))}.json'), encodings='UTF-8')
 
     def gmaps(self) -> object:
-        return googlemaps.Client(key='AIzaSyBzySrwJ9vArM857IgwQK1OBPXRt-DI38U')
+        return googlemaps.Client(key='')
 
 '''
-****************************************************************************************************
+********************************* crime in seoul ***************************************************************
 1. Target type 
  <class 'pandas.core.frame.DataFrame'> 
 2. Target column 
@@ -96,6 +98,44 @@ class Reader(ReaderBase):
 폭력 발생    0
 폭력 검거    0
 dtype: int64개
+****************************************************************************************************
+'''
+
+'''
+************************************ cctv in seoul *************************************************
+1. Target type 
+ <class 'pandas.core.frame.DataFrame'> 
+2. Target column 
+ Index(['기관명', '소계', '2013년도 이전', '2014년', '2015년', '2016년'], dtype='object') 
+3. Target top 1개 행
+    기관명    소계  2013년도 이전  2014년  2015년  2016년
+0  강남구  2780       1292    430    584    932 
+4. Target bottom 1개 행
+     기관명   소계  2013년도 이전  2014년  2015년  2016년
+24  중랑구  660        509    121    177    109 
+5. Target null 의 갯수
+ 기관명          0
+소계           0
+2013년도 이전    0
+2014년        0
+2015년        0
+2016년        0
+dtype: int64개
+****************************************************************************************************
+'''
+
+'''
+******************************** pop in seoul *******************************************************
+1. Target type 
+ <class 'pandas.core.frame.DataFrame'> 
+2. Target column 
+ Index(['자치구', '계', '계.1', '계.2', '65세이상고령자'], dtype='object') 
+3. Target top 1개 행
+   자치구           계        계.1       계.2   65세이상고령자
+0  합계  10197604.0  9926968.0  270636.0  1321458.0 
+4. Target bottom 1개 행
+     자치구   계  계.1  계.2  65세이상고령자
+26  NaN NaN  NaN  NaN       NaN 
 ****************************************************************************************************
 '''
 
@@ -132,11 +172,80 @@ class Service(Reader):
         for name in crime['관서명']:
             station_names.append('Seoul' + str(name[:-1] + '경찰서'))
         station_addrs = []
-        station_lat = []
+        station_lats = []
         station_lngs = []
         gmaps = reader.gmaps()
+        for name in station_names:
+            temp = gmaps.geocode(name, language='ko')
+            station_addrs.append(temp[0].get('formatted_address'))
+            t_loc = temp[0].get('geometry')
+            station_lats.append(t_loc['location']['lat'])
+            station_lngs.append(t_loc['location']['lng'])
+            print(f'name{temp[0].get("formatted_address")}')
+        gu_names = []
+        for name in station_addrs:
+            t = name.split()
+            gu_name = [gu for gu in t if gu[-1] == '구'][0]
+            gu_names.append(gu_name)
+        crime['구별'] = gu_names
+        # 구와 경찰서 위치가 다른 경우 수작업
+        crime.loc[crime['관서명'] == '혜화서', ['구별']] == '종로구'
+        crime.loc[crime['관서명'] == '서부서', ['구별']] == '은평구'
+        crime.loc[crime['관서명'] == '강서서', ['구별']] == '양천구'
+        crime.loc[crime['관서명'] == '종암서', ['구별']] == '성북구'
+        crime.loc[crime['관서명'] == '방배서', ['구별']] == '서초구'
+        crime.loc[crime['관서명'] == '수서서', ['구별']] == '강남구'
+        crime.to_csv('./saved_data/police_pos.csv')
+
+    def save_cctv_pop(self):
+        file = self.file
+        reader = self.reader
+        printer = self.printer
+        file.context = './data/'
+        file.fname = 'cctv_in_seoul'
+        cctv = reader.csv(file)
+        # printer.dframe(cctv)
+        file.fname = 'pop_in_seoul'
+        pop = reader.xls(file, 2, 'B, D, G, J, N')
+        # printer.dframe(pop)
+        cctv.rename(columns={cctv.columns[0]: '구별'}, inplace=True)
+        pop.rename(columns={
+            pop.columns[0]: '구별',
+            pop.columns[1]: '인구수',
+            pop.columns[2]: '한국인',
+            pop.columns[3]: '외국인',
+            pop.columns[4]: '고령자'
+        }, inplace=True)
+        pop.drop([26], inplace=True)
+        pop['외국인비율'] = pop['외국인'].astype(int) / pop['인구수'].astype(int) * 100
+        pop['고령자비율'] = pop['고령자'].astype(int) / pop['인구수'].astype(int) * 100
+        cctv.drop(['2013년도 이전', '2014년', '2015년', '2016년'], 1, inplace=True)
+        cctv_pop = pd.merge(cctv, pop, on='구별')
+        cor1 = np.corrcoef(cctv_pop['고령자비율'], cctv_pop['소계'])
+        cor2 = np.corrcoef(cctv_pop['외국인비율'], cctv_pop['소계'])
+        print(f'고령자비율과 CCTV의 상관계수 {str(cor1)} \n'
+              f'외국인비율과 CCTV의 상관계수 {str(cor2)} ')
+        """
+         고령자비율과 CCTV 의 상관계수 [[ 1.         -0.28078554]
+                                     [-0.28078554  1.        ]] 
+         외국인비율과 CCTV 의 상관계수 [[ 1.         -0.13607433]
+                                     [-0.13607433  1.        ]]
+        r이 -1.0과 -0.7 사이이면, 강한 음적 선형관계,
+        r이 -0.7과 -0.3 사이이면, 뚜렷한 음적 선형관계,
+        r이 -0.3과 -0.1 사이이면, 약한 음적 선형관계,
+        r이 -0.1과 +0.1 사이이면, 거의 무시될 수 있는 선형관계,
+        r이 +0.1과 +0.3 사이이면, 약한 양적 선형관계,
+        r이 +0.3과 +0.7 사이이면, 뚜렷한 양적 선형관계,
+        r이 +0.7과 +1.0 사이이면, 강한 양적 선형관계
+        고령자비율 과 CCTV 상관계수 [[ 1.         -0.28078554] 약한 음적 선형관계
+                                    [-0.28078554  1.        ]]
+        외국인비율 과 CCTV 상관계수 [[ 1.         -0.13607433] 거의 무시될 수 있는
+                                    [-0.13607433  1.        ]]                        
+         """
+        cctv_pop.to_csv('./saved_data/cctv_pop.csv')
 
 if __name__ == '__main__':
 
     s = Service()
-    s.save_police_pos()
+    # s.save_police_pos()
+    s.save_cctv_pop()
